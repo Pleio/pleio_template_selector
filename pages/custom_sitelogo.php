f145dd3ea6104465d9664441126309af2d228f07
Jerome Bakker <jeabakker@coldtrick.com>
2013-04-09 12:13:40 +0200

    fixed: PHP warning file not found

--- pages/custom_sitelogo.php
+++ pages/custom_sitelogo.php
@@ -1,18 +1,18 @@
 <?php 
 
-	global $CONFIG;
+	$file_location = elgg_get_config("dataroot") . "pleio_template_selector/site_logos/logo_" . elgg_get_site_entity()->getGUID();
 	
-	if($contents = file_get_contents($CONFIG->dataroot . "pleio_template_selector/site_logos/logo_" . $CONFIG->site_guid)){
-		header("Content-type: image/jpeg");
-		header('Expires: ' . date('r',time() + 864000));
-		header("Pragma: public");
-		header("Cache-Control: public");
-		header("Content-Length: " . strlen($contents));
-		
-		$splitString = str_split($contents, 1024);
-		foreach($splitString as $chunk) {
-			echo $chunk;
+	if(file_exists($file_location)){
+		if($contents = file_get_contents($file_location)){
+			header("Content-type: image/jpeg");
+			header('Expires: ' . date('r',time() + 864000));
+			header("Pragma: public");
+			header("Cache-Control: public");
+			header("Content-Length: " . strlen($contents));
+			
+			$splitString = str_split($contents, 1024);
+			foreach($splitString as $chunk) {
+				echo $chunk;
+			}
 		}
 	}
-
-?>
