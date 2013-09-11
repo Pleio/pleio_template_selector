4eac9e7d1be1b5b12bf9378db79590e8d2c5c3de
Jeroen Dalsem <jdalsem@coldtrick.com>
2013-09-11 09:28:30 +0200

    fixed dropdown menu have unwanted offset

--- views/default/pleio_template_selector/custom_css.php
+++ views/default/pleio_template_selector/custom_css.php
@@ -1,8 +1,14 @@
 <?php
 // custom header height
-$layout_header_height = (int) elgg_get_plugin_setting("layout_header_height", "pleio_template_selector");
+$layout_header_height = sanitise_int(elgg_get_plugin_setting("layout_header_height", "pleio_template_selector"), false);
 $show_logo_footer = elgg_get_plugin_setting("show_logo_footer", "pleio_template_selector");
 $disable_rounded_corners = elgg_get_plugin_setting("disable_rounded_corners", "pleio_template_selector");
+$menu_bottom = sanitise_int(elgg_get_plugin_setting("menu_bottom", "pleio_template_selector"), false);
+$menu_align = elgg_get_plugin_setting("menu_align", "pleio_template_selector");
+
+if (empty($menu_align) || !in_array($menu_align, array("left", "right"))) {
+	$menu_align = "left";
+}
 
 if (!empty($layout_header_height)) {
 	?>
@@ -20,7 +26,6 @@ if ($show_logo_footer == "no") {
 	<?php
 }
 
-
 if ($disable_rounded_corners != "no") {
 	?>
 	* {
@@ -30,6 +35,27 @@ if ($disable_rounded_corners != "no") {
 	}
 	<?php
 }
+
+?>
+.elgg-menu-site-container {
+	background: #<?php echo THEME_COLOR_1; ?>;
+	bottom: <?php echo $menu_bottom; ?>px;
+    height: 45px;
+    left: 0;
+    position: absolute;
+    width: 100%;
+}
+
+.elgg-menu-site-container .elgg-menu-site-default {
+	<?php
+	if ($menu_align == "left") {
+		echo "left: 20px;";
+	} else {
+		echo "right: 20px;";
+	}
+	?>
+}
+<?php
 
 
 
