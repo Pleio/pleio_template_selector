99cf0468ef9780087efe683e35af1d76204d176b
Jeroen Dalsem <jdalsem@coldtrick.com>
2013-09-23 11:15:33 +0200

    added etag caching of site images

--- pages/custom_sitelogo.php
+++ pages/custom_sitelogo.php
@@ -1,4 +1,12 @@
-<?php 
+<?php
+	global $CONFIG;
+	
+	$etag = (int) $CONFIG->lastcache;
+	
+	if (isset($_SERVER['HTTP_IF_NONE_MATCH']) && trim($_SERVER['HTTP_IF_NONE_MATCH']) == "\"$etag\"") {
+		header("HTTP/1.1 304 Not Modified");
+		exit;
+	}
 
 	$file_location = elgg_get_config("dataroot") . "pleio_template_selector/site_logos/logo_" . elgg_get_site_entity()->getGUID();
 	
@@ -8,6 +16,7 @@
 			header('Expires: ' . date('r',time() + 864000));
 			header("Pragma: public");
 			header("Cache-Control: public");
+			header("ETag: \"$etag\"");
 			header("Content-Length: " . strlen($contents));
 			
 			$splitString = str_split($contents, 1024);
