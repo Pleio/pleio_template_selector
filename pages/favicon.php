b202c4bdaae24a619be73a8055c439908110a22c
Jeroen Dalsem <jdalsem@coldtrick.com>
2013-12-06 15:21:03 +0100

    fixed caching of favicon

--- pages/favicon.php
+++ pages/favicon.php
@@ -1,14 +1,5 @@
 <?php
 
-	global $CONFIG;
-	
-	$etag = (int) $CONFIG->lastcache;
-	
-	if (isset($_SERVER['HTTP_IF_NONE_MATCH']) && trim($_SERVER['HTTP_IF_NONE_MATCH']) == "\"$etag\"") {
-		header("HTTP/1.1 304 Not Modified");
-		exit;
-	}
-	
 	$file_location = elgg_get_config("dataroot") . "pleio_template_selector/favicon/favicon_" . elgg_get_site_entity()->getGUID();
 	
 	if (!file_exists($file_location)) {
@@ -21,7 +12,6 @@
 			header('Expires: ' . date('r',time() + 864000));
 			header("Pragma: public");
 			header("Cache-Control: public");
-			header("ETag: \"$etag\"");
 			header("Content-Length: " . strlen($contents));
 				
 			$splitString = str_split($contents, 1024);
--- views/default/page/elements/shortcut_icon.php
+++ views/default/page/elements/shortcut_icon.php
@@ -2,5 +2,7 @@
 /**
  * Displays the default shortcut icon
  */
+
+$lastcache = elgg_get_config("lastcache");
 ?>
-<link rel="SHORTCUT ICON" href="<?php echo elgg_get_site_url(); ?>template_selector/favicon" />
+<link rel="SHORTCUT ICON" href="<?php echo elgg_get_site_url(); ?>template_selector/favicon?lc=<?php echo $lastcache; ?>" />
